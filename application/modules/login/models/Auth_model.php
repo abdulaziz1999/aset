5c764968bdeba101b70524c33260b603f8007370
abdulaziz1999 <azizmentor96@gmail.com>
2022-03-06 18:02:11 +0700

    security

--- application/modules/login/models/Auth_model.php
+++ application/modules/login/models/Auth_model.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 Class Auth_model extends CI_Model{
 
     function pasUser($u){
-      $data = $this->db->get_where('tb_user',['email' => $u])->row()->password;
+      @$data = $this->db->get_where('tb_user',['email' => $u])->row()->password;
       if($data){
         return $data;
       }else{
@@ -17,8 +17,7 @@ Class Auth_model extends CI_Model{
     }
 
     function nama_user($u){
-      $data = $this->db->get_where('tb_user',['email' => $u])->row()->nama;
-      return $data;
+      @$data = $this->db->get_where('tb_user',['email' => $u])->row()->nama;
       if($data){
         return $data;
       }else{
